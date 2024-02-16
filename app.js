7750241c664b2897a5d467970fdb8253cc9bc09c
Vasudha2407 <160100131+Vasudha2407@users.noreply.github.com>
2024-02-16 15:34:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World team 3 goooood!'));
+app.get('/', (req, res) => res.send('team 3 this is vasudha!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
